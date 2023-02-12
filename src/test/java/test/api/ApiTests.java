ac59f2101127674dd66a28ea0266602b03c4375c
andrewnnov <andrewnnov@yandex.ru>
2023-02-12 09:13:40 -0300

    init

---
+++ src/test/java/test/api/ApiTests.java
@@ -0,0 +1,56 @@
+package test.api;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.Matchers.*;
+
+public class ApiTests {
+
+    @Test
+    public void firstTest() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().all()
+                .body("page", notNullValue())
+                .body("per_page", notNullValue())
+                .body("total", notNullValue())
+                .body("total_pages", notNullValue())
+                .body("data.id", not(hasItem(nullValue())))
+                .body("data.first_name", hasItem("Lindsay"));
+    }
+
+    @Test
+    public void secondTest() {
+        Map<String, String> requestData = new HashMap<>();
+        requestData.put("name", "Valdemar");
+        requestData.put("job", "Shut");
+        Response response = given()
+                .contentType("application/json")
+                .body(requestData)
+                .log().all()
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .response();
+
+        JsonPath jsonResponse = response.jsonPath();
+        Assert.assertEquals(jsonResponse.get("name"), requestData.get("name")
+                , "Ожидали создание пользователя с именем "
+                        + requestData.get("name")
+                        + ", создался с именем" + jsonResponse.get("name"));
+    }
+
+}
